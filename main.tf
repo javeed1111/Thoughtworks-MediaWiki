337657d08d54c619a32b36418a7688c00e676af4
javeedfedal <103515308+javeed1111@users.noreply.github.com>
2024-03-25 14:00:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,19 +5,14 @@ provider "aws" {
 terraform {
   required_version = ">= 0.12"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"  
-    }
+    aws = "~> 3.0"  
   }
 }
 
 module "eks_cluster" {
   source = "terraform-aws-modules/eks/aws"
-  version = "17.16.0"  # Specify the version of the EKS module
-
   cluster_name = "eks_cluster_for_MediaWiki"
-  cluster_version = "1.21"
+  cluster_version = "1.20"  # Specify a supported Kubernetes version
   subnets = ["subnet-0249a2c08da5e58cb", "subnet-00b22146f5849348c"]  
   vpc_id = "vpc-0b73766d2f01cb46b" 
   node_groups = {
